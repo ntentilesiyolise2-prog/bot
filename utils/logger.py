5d62b7dc1b3e8eb6c138db4728ee7c1216e50587
Siyolise Ntentile <ntentilesiyolise2@gmail.com>
2026-08-11 19:10:13 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+def setup_logger(name: str, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+    return logger
